1fb2fd7245f767ff9fd1589d783a4246983d90a9
Danny Solsman <danny@dannysolsman.com>
2017-08-05 18:12:32 -0400

    Add files via upload

---
+++ step3/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$hername = 'Jane Doe';
+$greeting = 'You have amazing taste in music.';
+$myname = 'Danny';
+$shmooze ='Can I buy you a drink?';
+
+echo 'Hello, ' . $hername . '.' . $greeting;
+echo ' ' . 'My name is ' . $myname . '.';
+echo ' ' . $shmooze;

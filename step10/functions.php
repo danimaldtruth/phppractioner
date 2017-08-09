cbe6741d9402864d85e660823333f5030c6efd41
Danny Solsman <danny@dannysolsman.com>
2017-08-09 00:23:31 -0400

    need some help on this one......

---
+++ step10/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+$guestage = 30;
+$minage = 21;
+
+function oldenough() { if ($guestage < $minage)
+
+					echo "You got some cycles around the sun to go through kid. Come back when you're a little more experienced.";
+
+					}
+
+					
+?>
---
+++ step10/index.tmpl.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>You old enough?</title>
+
+	
+</head>
+
+<body>
+ <header>
+ </header>
+
+	
+<body>
+	<h1>Hmmmmmm are you old enough? Let me check..........</h1>
+
+<?php function oldenough() ?>
+
+</html>

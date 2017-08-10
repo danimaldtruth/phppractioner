970d073218a1b7ebcf7cd4df9f49edf5c57a89eb
Danny Solsman <danny@dannysolsman.com>
2017-08-09 22:06:35 -0400

    success!

--- step10/functions.php
+++ step10/functions.php
@@ -1,21 +1,17 @@
 <?php
 
-$guestage = 30;
+$guestage = 12;
 $minage = 21;
 
-function oldenough() { if ($guestage < $minage)
+ function oldenough() 
+{ 
+    $guestage = 12;
+    $minage = 21;
 
-					echo "You got some cycles around the sun to go through kid. Come back when you're a little more experienced.";
+    if ($guestage < $minage) {
+                    echo "You got some cycles around the sun to go through kid. Come back when you're a little more experienced.";
+    }    else {
+        echo "Welcome to Jelly Bear's Island of Sweets. The greatest club this side of rainbow road.";
+    }
+} 
 
-					}
-
-						
-
-					else 
-
-
-						echo "Welcome to Jelly Bear's Island of Sweets. The greatest club this side of rainbow road.";
-					}
-
-
-?>
